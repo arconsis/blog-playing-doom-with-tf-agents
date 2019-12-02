50e0c8b2fe84dc15cbd3fcdfc13ece62c0737db2
Andreas Eberle <andreas.eberle@arconsis.com>
2019-12-02 15:58:00 +0100

    Adapt doom extended trainer to newer tf-agents version

--- ppo_train_eval_doom_extended.py
+++ ppo_train_eval_doom_extended.py
@@ -1,7 +1,3 @@
-from __future__ import absolute_import
-from __future__ import division
-from __future__ import print_function
-
 import os
 import time
 
@@ -11,38 +7,28 @@ from absl import flags
 from absl import logging
 from tf_agents.agents.ppo import ppo_agent
 from tf_agents.drivers import dynamic_episode_driver
-from tf_agents.environments import parallel_py_environment
-from tf_agents.environments import tf_py_environment
+from tf_agents.environments import tf_py_environment, parallel_py_environment
 from tf_agents.eval import metric_utils
 from tf_agents.metrics import tf_metrics
 from tf_agents.networks.actor_distribution_rnn_network import ActorDistributionRnnNetwork
 from tf_agents.networks.value_rnn_network import ValueRnnNetwork
+from tf_agents.policies import policy_saver
 from tf_agents.replay_buffers import tf_uniform_replay_buffer
 from tf_agents.utils import common
 
 from doom.DoomEnvironment import DoomEnvironment
 from utils.visualization_helper import create_video
 
+
 flags.DEFINE_string('root_dir', os.getenv('TEST_UNDECLARED_OUTPUTS_DIR'), 'Root directory for writing summaries and checkpoints')
 FLAGS = flags.FLAGS
 
 
 def create_networks(tf_env):
-	actor_net = ActorDistributionRnnNetwork(
-		tf_env.observation_spec(),
-		tf_env.action_spec(),
-		conv_layer_params=[(16, 8, 4), (32, 4, 2)],
-		input_fc_layer_params=(256,),
-		lstm_size=(256,),
-		output_fc_layer_params=(128,),
-		activation_fn=tf.nn.elu)
-	value_net = ValueRnnNetwork(
-		tf_env.observation_spec(),
-		conv_layer_params=[(16, 8, 4), (32, 4, 2)],
-		input_fc_layer_params=(256,),
-		lstm_size=(256,),
-		output_fc_layer_params=(128,),
-		activation_fn=tf.nn.elu)
+	actor_net = ActorDistributionRnnNetwork(tf_env.observation_spec(), tf_env.action_spec(), conv_layer_params=[(16, 8, 4), (32, 4, 2)], input_fc_layer_params=(256,), lstm_size=(256,),
+											output_fc_layer_params=(128,))
+	value_net = ValueRnnNetwork(tf_env.observation_spec(), conv_layer_params=[(16, 8, 4), (32, 4, 2)], input_fc_layer_params=(256,), lstm_size=(256,), output_fc_layer_params=(128,),
+								activation_fn=tf.nn.elu)
 
 	return actor_net, value_net
 
@@ -51,8 +37,8 @@ def train_eval_doom(
 		root_dir,
 		# Params for collect
 		num_environment_steps=30000000,
-		collect_episodes_per_iteration=32,
-		num_parallel_environments=32,
+		collect_episodes_per_iteration=6,
+		num_parallel_environments=36,
 		replay_buffer_capacity=301,  # Per-environment
 		# Params for train
 		num_epochs=25,
@@ -66,15 +52,13 @@ def train_eval_doom(
 		log_interval=50,
 		summary_interval=50,
 		summaries_flush_secs=1,
-		use_tf_functions=False):
+		use_tf_functions=True):
 	"""A simple train and eval for PPO."""
-
-	if root_dir is None:
-		raise AttributeError('train_eval requires a root_dir.')
 
 	root_dir = os.path.expanduser(root_dir)
 	train_dir = os.path.join(root_dir, 'train')
 	eval_dir = os.path.join(root_dir, 'eval')
+	saved_model_dir = os.path.join(root_dir, 'policy_saved_model')
 
 	train_summary_writer = tf.compat.v2.summary.create_file_writer(train_dir, flush_millis=summaries_flush_secs * 1000)
 	train_summary_writer.set_as_default()
@@ -86,8 +70,10 @@ def train_eval_doom(
 	]
 
 	global_step = tf.compat.v1.train.get_or_create_global_step()
+
 	with tf.compat.v2.summary.record_if(lambda: tf.math.equal(global_step % summary_interval, 0)):
 		eval_py_env = DoomEnvironment()
+
 		eval_tf_env = tf_py_environment.TFPyEnvironment(eval_py_env)
 		tf_env = tf_py_environment.TFPyEnvironment(parallel_py_environment.ParallelPyEnvironment([DoomEnvironment] * num_parallel_environments))
 		optimizer = tf.compat.v1.train.AdamOptimizer(learning_rate=learning_rate, epsilon=1e-5)
@@ -101,8 +87,6 @@ def train_eval_doom(
 			actor_net=actor_net,
 			value_net=value_net,
 			num_epochs=num_epochs,
-			train_step_counter=global_step,
-			discount_factor=0.99,
 			gradient_clipping=0.5,
 			entropy_regularization=1e-2,
 			importance_ratio_clipping=0.2,
@@ -118,15 +102,20 @@ def train_eval_doom(
 		]
 
 		train_metrics = step_metrics + [
-			tf_metrics.AverageReturnMetric(),
-			tf_metrics.AverageEpisodeLengthMetric(),
+			tf_metrics.AverageReturnMetric(batch_size=num_parallel_environments),
+			tf_metrics.AverageEpisodeLengthMetric(batch_size=num_parallel_environments),
 		]
 
 		eval_policy = tf_agent.policy
 		collect_policy = tf_agent.collect_policy
 
+		replay_buffer = tf_uniform_replay_buffer.TFUniformReplayBuffer(
+			tf_agent.collect_data_spec,
+			batch_size=num_parallel_environments,
+			max_length=replay_buffer_capacity)
+
 		train_checkpointer = common.Checkpointer(
-			ckpt_dir=os.path.join(train_dir, 'trainer'),
+			ckpt_dir=train_dir,
 			agent=tf_agent,
 			global_step=global_step,
 			metrics=metric_utils.MetricsGroup(train_metrics, 'train_metrics'))
@@ -134,11 +123,9 @@ def train_eval_doom(
 			ckpt_dir=os.path.join(train_dir, 'policy'),
 			policy=eval_policy,
 			global_step=global_step)
+		saved_model = policy_saver.PolicySaver(eval_policy, train_step=global_step)
 
-		replay_buffer = tf_uniform_replay_buffer.TFUniformReplayBuffer(
-			tf_agent.collect_data_spec,
-			batch_size=num_parallel_environments,
-			max_length=replay_buffer_capacity)
+		train_checkpointer.initialize_or_restore()
 
 		collect_driver = dynamic_episode_driver.DynamicEpisodeDriver(
 			tf_env,
@@ -159,8 +146,8 @@ def train_eval_doom(
 
 		if use_tf_functions:
 			# TODO(b/123828980): Enable once the cause for slowdown was identified.
-			collect_driver.run = common.function(collect_driver.run, autograph=False)
-			tf_agent.train = common.function(tf_agent.train, autograph=False)
+			collect_driver.run = common.function(collect_driver.run, autograph=True)
+			tf_agent.train = common.function(tf_agent.train, autograph=True)
 			train_step = common.function(train_step)
 
 		collect_time = 0
@@ -168,7 +155,6 @@ def train_eval_doom(
 		timed_at_step = global_step.numpy()
 
 		while environment_steps_metric.result() < num_environment_steps:
-
 			start_time = time.time()
 			collect_driver.run()
 			collect_time += time.time() - start_time
@@ -188,6 +174,7 @@ def train_eval_doom(
 				steps_per_sec = ((global_step_val - timed_at_step) / (collect_time + train_time))
 				logging.info('%.3f steps/sec', steps_per_sec)
 				logging.info('collect_time = {}, train_time = {}'.format(collect_time, train_time))
+				
 				with tf.compat.v2.summary.record_if(True):
 					tf.compat.v2.summary.scalar(name='global_steps_per_sec', data=steps_per_sec, step=global_step)
 
@@ -195,20 +182,25 @@ def train_eval_doom(
 				collect_time = 0
 				train_time = 0
 
-			if global_step_val % eval_interval == 0:
+			if global_step_val % eval_interval == 0 and global_step_val > 0:
 				evaluate()
 
 			if global_step_val % checkpoint_interval == 0:
 				train_checkpointer.save(global_step=global_step_val)
 				policy_checkpointer.save(global_step=global_step_val)
+				saved_model_path = os.path.join(saved_model_dir, 'policy_' + ('%d' % global_step_val).zfill(9))
+				saved_model.save(saved_model_path)
 
 		# One final eval before exiting.
 		evaluate()
 
 
 def main(_):
-	tf.compat.v1.enable_v2_behavior()  # For TF 1.x users
 	logging.set_verbosity(logging.INFO)
+
+	if FLAGS.root_dir is None:
+		raise AttributeError('train_eval requires a root_dir.')
+
 	train_eval_doom(root_dir=FLAGS.root_dir)
 
 
